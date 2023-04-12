8ed7478709550633fa027493bad115349bc95d99
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-04-12 15:30:08 +0700

    Create home component

---
+++ components/home/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <!-- wrapper  -->
+  <div class="flex flex-col items-start p-4 gap-4">
+    <!-- pict profile -->
+    <div
+      class="flex flex-row items-center gap-[.938rem] pb-[.75rem] w-[15.563rem] h-[6.375rem]"
+    >
+      <!-- picture -->
+      <picture class="w-[90px] h-[90px]">
+        <img src="../../assets/img/Hexagon.svg" alt="Profile" />
+      </picture>
+      <div class="">
+        <h1 class="text-lg text-black font-medium">Dimas Okta</h1>
+        <!-- wrapper -->
+        <div class="flex flex-row items-center gap-1">
+          <p class="text-base font-semibold text-[#4A576F] opacity-70">
+            realvjy.eth
+          </p>
+          <svg
+            width="17"
+            height="16"
+            viewBox="0 0 17 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5.66488 4C5.29669 4 4.99821 4.29848 4.99821 4.66667C4.99821 5.03485 5.29669 5.33333 5.66488 5.33333V4ZM12.3315 4.66667H12.9982C12.9982 4.29848 12.6997 4 12.3315 4V4.66667ZM11.6649 11.3333C11.6649 11.7015 11.9634 12 12.3315 12C12.6997 12 12.9982 11.7015 12.9982 11.3333H11.6649ZM4.52681 11.5286C4.26645 11.7889 4.26645 12.2111 4.52681 12.4714C4.78716 12.7318 5.20927 12.7318 5.46961 12.4714L4.52681 11.5286ZM5.66488 5.33333H12.3315V4H5.66488V5.33333ZM11.6649 4.66667V11.3333H12.9982V4.66667H11.6649ZM11.8601 4.19527L4.52681 11.5286L5.46961 12.4714L12.8029 5.13807L11.8601 4.19527Z"
+              fill="#4A576F"
+              fill-opacity="0.7"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <!-- deskripsi -->
+    <p
+      class="font-normal text-[20px] leading-8 tracking-[-1.7%] text-[#22242C]"
+    >
+      A design wizard voyaging into the metaverse. I tell the story through my
+      design and illustrations. I spent most of my time designing for brands and
+      creating open-source design resources.
+    </p>
+    <!-- footer text -->
+    <div class="flex flex-row items-start gap-[9px] w-[344px] h-[24px]">
+      <h1 class="font-medium text-lg text-[#4A576F] opacity-70 pr-2 lg:pr-0">
+        Building what I love at
+        <span
+          class="opacity-100 font-semibold text-lg text-transparent bg-clip-text bg-gradient-to-r from-[#4AB1F1] to-[#FF7C51]"
+        >
+          @overlayz
+        </span>
+        <span class="text-[#4A576F] opacity-70 font-medium text-lg"
+          >studio</span
+        >
+      </h1>
+    </div>
+  </div>
+</template>
+<script></script>
