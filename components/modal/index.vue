1fd6ee044d3887493d2859858e9eeaa625bfd999
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-04-21 19:58:51 +0700

    Create modal component with swipper

---
+++ components/modal/index.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ModalsContainer, VueFinalModal } from "vue-final-modal";
+const getInitialValues = () => ({
+  teleportTo: "body",
+  modelValue: false,
+  displayDirective: "if",
+  hideOverlay: false,
+  overlayTransition: "vfm-fade",
+  contentTransition: "vfm-fade",
+  clickToClose: true,
+  escToClose: true,
+  background: "non-interactive",
+  lockScroll: true,
+  swipeToClose: "none",
+});
+const options = ref(getInitialValues());
+function reset() {
+  options.value = getInitialValues();
+}
+</script>
+<template>
+  <div class="mt-4 space-x-4">
+    <button class="ml-auto" @click="options.modelValue = true">
+      Open modal
+    </button>
+    <button @click="reset">Reset</button>
+  </div>
+  <VueFinalModal
+    v-model="options.modelValue"
+    :teleport-to="options.teleportTo"
+    :display-directive="options.displayDirective"
+    :hide-overlay="options.hideOverlay"
+    :overlay-transition="options.overlayTransition"
+    :content-transition="options.contentTransition"
+    :click-to-close="options.clickToClose"
+    :esc-to-close="options.escToClose"
+    :background="options.background"
+    :lock-scroll="options.lockScroll"
+    :swipe-to-close="options.swipeToClose"
+    class="flex items-center justify-center"
+    content-class="max-w-xl p-4 mx-4 space-y-2 bg-white border rounded-lg dark:bg-gray-900 dark:border-gray-700"
+  >
+    <h1 class="text-xl">Hello World!</h1>
+    <p>
+      Magna deserunt nulla aliquip velit aute. Et occaecat elit nulla excepteur
+      labore cupidatat. Duis culpa mollit commodo dolor qui Lorem qui laborum
+      elit elit Lorem occaecat. Commodo eiusmod esse voluptate officia amet quis
+      occaecat aliqua. Proident do irure amet ut occaecat dolor laboris
+      consectetur.
+    </p>
+    <button @click="options.modelValue = false">Close</button>
+  </VueFinalModal>
+  <ModalsContainer />
+</template>
+<style scoped lang="css">
+button {
+  @apply px-2 py-1 border rounded;
+}
+</style>
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,12 +1,13 @@
 <script setup></script>
 <template>
   <div
-    class="scrolling transition-all duration-500 bg-top bg-no-repeat bg-cover bg-lightMb dark:bg-darkMb lg:dark:bg-Dark lg:bg-Light"
+    class="transition-all duration-500 bg-top bg-no-repeat bg-cover scrolling bg-lightMb dark:bg-darkMb lg:dark:bg-Dark lg:bg-Light"
   >
     <navbar></navbar>
     <main class="container">
       <workme></workme>
       <slide></slide>
+      <!-- <modal></modal> -->
       <footerr></footerr>
     </main>
   </div>
