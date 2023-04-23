5be156e424a35cae4c47d4722ac98ebc30238a48
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-04-23 20:59:07 +0700

    fix scroll for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <script setup></script>
 <template>
-  <div
-    class="scrolling transition-all duration-500 bg-top bg-no-repeat bg-cover bg-lightMb dark:bg-darkMb lg:dark:bg-Dark lg:bg-Light"
-  >
+  <div class="pages lg:scrolling">
     <navbar></navbar>
     <main class="container">
       <home></home>
