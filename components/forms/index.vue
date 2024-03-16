3269c4d1ea49ebc04128d39f61af34cab861ff0b
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-03-16 22:38:39 +0700

    test: testing submit-2

--- components/forms/index.vue
+++ components/forms/index.vue
@@ -26,8 +26,15 @@ async function sendEmail(value) {
         form: [[value.name], [value.email], [value.msg]],
       }),
     });
-    const result = await response.data.value
-    if (result === "Email sent successfully!") return loadingError.value = true
+    return response.data
+    // if (response && response.data) {
+    //   loadingError.value = true
+    //   console.log(response.data);
+    //   return response.data
+    // } else {
+    //   console.error("Invalid response format:", response.data);
+    // }
+    // if (result === "Email sent successfully!") return loadingError.value = true
   } catch (err) {
     console.warn(err);
   } finally {
